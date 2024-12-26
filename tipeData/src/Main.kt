be2b23a216b77d9bd2dd6260d486f4a256133849
luckyMAUL <luckyggws@gmail.com>
2024-12-26 09:45:03 +0700

    Add files via upload

---
+++ tipeData/src/Main.kt
@@ -0,0 +1,27 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val byteVal: Byte = 127
+    val shortVal: Short = 32000
+    val intVal: Int = 2147483647
+    val longVal: Long = 9223372036854775807
+
+    //====Bilangan Bulat=====
+//Byte: 8-bit, nilai: -128 hingga 127.
+//Short: 16-bit, nilai: -32,768 hingga 32,767.
+//Int: 32-bit, nilai: -2,147,483,648 hingga 2,147,483,647.
+//Long: 64-bit, nilai: -9,223,372,036,854,775,808 hingga 9,223,372,036,854,775,807./
+
+    val charVal: Char = 'A'
+    val name:String="agus"
+    val booleanVal: Boolean = true
+
+    //Karakter dan Boolean
+    //Char: Karakter Unicode tunggal, misalnya 'A', '9'.
+    //String: untuk menulis kalimat
+    //Boolean: Hanya memiliki dua nilai: true atau false.
+
+
+}
+
+
