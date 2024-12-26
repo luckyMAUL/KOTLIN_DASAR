b1add747d0f31151ac0f948eb6ece23ca9fb7cfc
luckyMAUL <luckyggws@gmail.com>
2024-12-26 09:48:43 +0700

    Add files via upload

---
+++ loop while and do while/src/do while.kt
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var i = 1
+
+    println("====DO WHILW=====")
+
+    do {
+        println("helo world $i")
+        i++
+
+    }while (i <= 7)
+
+    println("====WHILW=====")
+
+    var j = 1
+
+    while(j <= 7){
+        println("hello world $j")
+        j++
+    }
+}
+
---
+++ loop while and do while/src/while.kt
@@ -0,0 +1,9 @@
+fun main() {
+
+    var i = 0
+
+    while(i <= 7){
+        println("hello world $i")
+        i++
+    }
+}
