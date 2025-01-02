ebb2064fae82e9dfc81b5fc829c3c1da5c1cd1fd
luckyMAUL <luckyggws@gmail.com>
2025-01-02 17:22:29 +0700

    Add files via upload

---
+++ variable constant/src/Main.kt
@@ -0,0 +1,26 @@
+
+
+const val PI = 3.14
+const val Panjang = 12
+const val nama ="ucup"
+const val nilai ='A'
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    println(PI+2)
+    println(PI)
+
+
+//const di Kotlin digunakan untuk mendeklarasikan konstanta
+// compile-time yang nilainya tidak pernah berubah dan diketahui
+// pada saat kompilas
+
+
+}
+//Gunakan const ketika:
+//
+//Nilai tetap, diketahui pada saat kompilasi.
+//Tidak ada rencana untuk mengubah nilai tersebut selama runtime.
+//Anda ingin mendeklarasikan konstanta global, konfigurasi aplikasi,
+// atau nilai statis yang sering digunakan.
