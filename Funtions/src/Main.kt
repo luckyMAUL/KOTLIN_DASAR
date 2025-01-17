91b75034b50f1e18227094cece397e00c0303bf4
luckyMAUL <luckyggws@gmail.com>
2025-01-17 20:41:40 +0700

    Add files via upload

---
+++ Funtions/src/Main.kt
@@ -0,0 +1,21 @@
+
+private fun level(){
+
+    
+}
+
+
+fun main() {
+
+    fun persegiPanjang(panjang: Int,lebar: Int): Int{
+        return 2*(panjang+lebar)
+    }
+    fun sapaHallo(name: String): String{
+        return "hallo selmat datang $name"
+    }
+    println(persegiPanjang(10,10))
+    println(sapaHallo("budi"))
+    println(sapaHallo("agus"))
+
+
+}
